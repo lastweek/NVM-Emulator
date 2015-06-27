cc1b136b86aee620746e1c68df8b1b2df4f806ec
Shan Yizhou <553997109@qq.com>
2015-06-27 18:04:59 +0800

    up

--- pmu_uncore.c
+++ pmu_uncore.c
@@ -1,12 +1,12 @@
-/**
+/*
  *	Description
  *
  *	Intel Uncore Performance Monitoring Module.
  *
- *	Specially, designed for Intel Xeon Processor 5600 Series(Westmere).
- *	Westmere is a subset of Nehalem. Nehalem's facilities apply to Westmere.
- *	(Supported MSRs listed in Table 35-10, Table 35-11, Table 35-13 in SDM.)
- **/
+ *	Specially, it's designed for Xeon 5600 Series(Westmere).
+ *	Westmere is a subset of Nehalem(?).
+ *	Nehalem's facilities apply to Westmere.
+ */
 
 #include <linux/init.h>
 #include <linux/kernel.h>
@@ -21,37 +21,32 @@
 #include <asm/nmi.h>
 #include <asm/msr.h>
 
-#define MSR_UNCORE_PERF_GLOBAL_CTRL		0x391
-#define MSR_UNCORE_PERF_GLOBAL_STATUS	0x392
-#define MSR_UNCORE_PERF_GLOBAL_OVF_CTRL	0x393
+/* Nehalem Global Control Registers */
+#define NHM_UNCORE_PERF_GLOBAL_CTRL		0x391
+#define NHM_UNCORE_PERF_GLOBAL_STATUS	0x392
+#define NHM_UNCORE_PERF_GLOBAL_OVF_CTRL	0x393
 
-#define MSR_UNCORE_PMCO		0x3b0
-#define MSR_UNCORE_PMC1		0x3b1
-#define MSR_UNCORE_PMC2		0x3b2
-
-typedef unsigned char		u8;
-typedef unsigned short		u16;
-typedef unsigned int		u32;
-typedef unsigned long long	u64;
-
+/* Nehalem Perf Control Registers */
+#define NHM_UNCORE_PMCO					0x3b0
+#define NHM_UNCORE_PERFEVTSEL0			0X3C0
 
 //#################################################
 // MODULE PART
 //#################################################
 
 static int
-pmu_init(void)
+pmu_uncore_init(void)
 {
 	return 0;
 }
 
 static void
-pmu_exit(void)
+pmu_uncore_exit(void)
 {
 
 }
 
-module_init(pmu_init);
-module_exit(pmu_exit);
+module_init(pmu_uncore_init);
+module_exit(pmu_uncore_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Shan Yizhou");
